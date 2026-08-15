13d6883925b737e04d85960356575271f669b42a
EVENTo0 <iooctopuss@gmail.com>
2026-08-15 10:52:00 +0400

    security: revoke RPC access to source-chain trigger functions

---
+++ supabase/sql/source-chain-trigger-rpc-lockdown.sql
@@ -0,0 +1,3 @@
+revoke all on function public.smart_os_validate_release_candidate_approval_chain() from public, anon, authenticated;
+revoke all on function public.smart_os_validate_lane_provenance_source_chain() from public, anon, authenticated;
+revoke all on function public.smart_os_validate_signing_job_source_chain() from public, anon, authenticated;
